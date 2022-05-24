44423fd993c370e3b06b7d6e9c90ff624e3177f2
Sigma <sigmw@ctemplar.com>
2022-05-24 18:44:54 -0300

    removed emalloc and omptmizations

--- main.c
+++ main.c
@@ -3,16 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void* emalloc(size_t size) {
-  void *p;
-  p = malloc(size);
-  return p;
-}
 
-enum {
-  SYSLOG_ACTION_SIZE_BUFFER = 10,
-  SYSLOG_ACTION_READ_ALL = 3
-};
 void dmesg(const char* buf, size_t n) {
   const char *p = buf;
   ssize_t r;
@@ -22,11 +13,11 @@ void dmesg(const char* buf, size_t n) {
 int main() {
   int n;
   char *buf;
-  n = klogctl(SYSLOG_ACTION_SIZE_BUFFER, NULL, 0);
+  n = klogctl(10, NULL, 0);
   if(n < 0)
     perror("stderr");
-  buf = emalloc(n);
-  n = klogctl(SYSLOG_ACTION_READ_ALL, buf, n);
+  buf = malloc(n);
+  n = klogctl(3, buf, n);
   dmesg(buf, n);
   free(buf);
 }
